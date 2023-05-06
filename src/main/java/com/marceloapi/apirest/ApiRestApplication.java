9dcff1263edeab30a75f653d27d29e52cb335d49
marcelojbcosta <marcelomogueco@gmail.com>
2023-05-06 19:34:36 -0400

    Config pom.xml and app.properties

--- src/main/java/com/marceloapi/apirest/ApiRestApplication.java
+++ src/main/java/com/marceloapi/apirest/ApiRestApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ApirestApplication {
+public class ApiRestApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ApirestApplication.class, args);
+
+		SpringApplication.run(ApiRestApplication.class, args);
 	}
+
 
 }
