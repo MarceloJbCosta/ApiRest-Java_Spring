8d8744f7b89482fafd9f3d477c38a78a1f33ea2a
marcelojbcosta <marcelomogueco@gmail.com>
2023-05-15 21:12:23 -0400

    Add get, put and delete

--- src/main/java/com/marceloapi/apirest/controller/DepartmentController.java
+++ src/main/java/com/marceloapi/apirest/controller/DepartmentController.java
@@ -4,9 +4,10 @@ import com.marceloapi.apirest.entities.Department;
 import com.marceloapi.apirest.entities.Product;
 import com.marceloapi.apirest.repositories.DepartmentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.Arrays;
 import java.util.List;
@@ -25,4 +26,34 @@ public class DepartmentController {
 
         return list;
     }
+
+    @PostMapping
+    public Department addDepartment(@RequestBody Department department) {
+        return this.departmentRepository.save(department);
+    }
+    @PutMapping("/{id}")
+    public Department addDepartment(@PathVariable long id, @RequestBody Department derpartment ){
+
+        Department existingDepartment = departmentRepository.findById(id).orElse(null);
+
+        if(existingDepartment != null) {
+            existingDepartment.setName(derpartment.getName());
+
+            return departmentRepository.save(existingDepartment);
+        }else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Product nut found");
+        }
+
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteDepartment(@PathVariable("id") Long id) {
+        if (departmentRepository.existsById(id)) {
+            departmentRepository.deleteById(id);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
 }
--- src/main/java/com/marceloapi/apirest/controller/ProductController.java
+++ src/main/java/com/marceloapi/apirest/controller/ProductController.java
@@ -2,12 +2,14 @@ package com.marceloapi.apirest.controller;
 
 import com.marceloapi.apirest.entities.Department;
 import com.marceloapi.apirest.entities.Product;
+import com.marceloapi.apirest.repositories.DepartmentRepository;
 import com.marceloapi.apirest.repositories.ProductRepository;
 import jakarta.servlet.http.PushBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.Arrays;
 import java.util.List;
@@ -17,6 +19,7 @@ import java.util.List;
 public class ProductController {
     @Autowired
     private ProductRepository productRepository;
+    private DepartmentRepository departmentRepository;
 
     @GetMapping
     public List<Product> getObjects(){
@@ -27,4 +30,37 @@ public class ProductController {
         return list;
     }
 
-}
+    @PostMapping
+    public Product addProduct(@RequestBody Product product) {
+        return this.productRepository.save(product);
+    }
+
+
+    @PutMapping("/{id}")
+    public Product updateProduct(@PathVariable Long id, @RequestBody Product product) {
+        Product existingProduct = productRepository.findById(id).orElse(null);
+
+        if (existingProduct != null) {
+            existingProduct.setNome(product.getNome());
+            existingProduct.setPrice(product.getPrice());
+            Department department = departmentRepository.findById(product.getDepartment().getId()).orElse(null);
+            existingProduct.setDepartment(department);
+
+            return productRepository.save(existingProduct);
+        } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Product not found");
+        }
+    }
+
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteProduct(@PathVariable("id") Long id) {
+        if (productRepository.existsById(id)) {
+            productRepository.deleteById(id);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+
+    }
